d3a73b3e3a29355f1f2d398949d85cad945d3b03
Sannyaaa <hasangunners30@gmail.com>
2024-10-14 09:43:38 +0700

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+
+// import './sidebar.js';
+// import './charts.js';
+// import './dark-mode.js';
+import '../../node_modules/flowbite/dist/flowbite.js';
+// import 'flowbite';
+
+// Have the courage to follow your heart and intuition.
