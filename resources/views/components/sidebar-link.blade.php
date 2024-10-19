7b5446085b8dae4ac932f211a0d0cba88d17fee2
Sannyaaa <hasangunners30@gmail.com>
2024-10-19 20:00:52 +0700

    sidebar

---
+++ resources/views/components/sidebar-link.blade.php
@@ -0,0 +1,9 @@
+{{-- @props(['disabled' => false]) --}}
+
+{{-- <input @disabled($disabled) {{ $attributes->merge(['class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500']) }}> --}}
+<a href="{{ $link ?? '#' }}" class="flex items-center p-2 text-base text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
+    <span class="text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        {{ $slot }}
+    </span>
+    <span class="ml-3" sidebar-toggle-item>{{ $name }}</span>
+</a>
