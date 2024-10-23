e4c9c83bc4cdfb6f24577dc5c89b70354c37bb30
thufail <62276390+muhammadthufailghazanfar@users.noreply.github.com>
2024-10-23 14:14:14 +0700

    More pages

--- resources/views/livewire/layouts/user-sidebar.blade.php
+++ resources/views/livewire/layouts/user-sidebar.blade.php
@@ -32,23 +32,23 @@
               <i class="fa-regular fa-file-lines"></i>
               <x-slot name="content">
                 <li>
-                  <a href="#" class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700  dark:bg-gray-700 ">All</a>
+                  <a href="{{route('user.tryouts')}}" class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700  dark:bg-gray-700 ">All</a>
                 </li>
                 <li>
-                  <a href="#" class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 dark:bg-gray-700 ">Owned</a>
+                  <a href="{{route('user.tryouts.owned')}}" class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 dark:bg-gray-700 ">Owned</a>
                 </li>
               </x-slot>
             </x-sidebar-dropdown>
           </li>
           <li>
             <x-sidebar-dropdown id="dropdown-bimbel" name="Bimbel">
-              <i class="fa-regular fa-file-lines"></i>
+              <i class="fa-solid fa-clapperboard"></i>
               <x-slot name="content">
                 <li>
-                <a href="#" class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700  dark:bg-gray-700 ">All</a>
+                  <a href="{{route('user.tryouts')}}" class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700  dark:bg-gray-700 ">All</a>
                 </li>
                 <li>
-                  <a href="#" class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 dark:bg-gray-700 ">Owned</a>
+                  <a href="{{route('user.tryouts.owned')}}" class="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 dark:bg-gray-700 ">Owned</a>
                 </li>
               </x-slot>
             </x-sidebar-dropdown>
--- routes/user.php
+++ routes/user.php
@@ -7,9 +7,9 @@ Route::get('/dashboard', function () {
     return view('welcome');
 });
 
-Route::get('/tryouts', App\Livewire\User\Tryouts::class)->name('user.tryouts');
-Route::get('/tryouts/owned', App\Livewire\User\Tryouts::class)->name('user.tryouts.owned');
-Route::get('/tryouts', App\Livewire\User\Tryouts::class)->name('user.bimbels');
-Route::get('/tryouts/owned', App\Livewire\User\Tryouts::class)->name('user.tryouts.owned');
+Route::get('/tryout', App\Livewire\User\Tryouts::class)->name('tryouts');
+Route::get('/tryout/owned', App\Livewire\User\TryoutsOwned::class)->name('tryouts.owned');
+Route::get('/bimbel', App\Livewire\User\Bimbels::class)->name('bimbels');
+Route::get('/bimbel/owned', App\Livewire\User\BimbelsOwned::class)->name('bimbels.owned');
 
 
