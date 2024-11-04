a9f9c46f1bc5222d7eb605c3e5145f2c89f5b23e
faiqhsaaan <faiqihsan77@gmail.com>
2024-11-04 19:41:51 +0700

    seed

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -69,19 +69,9 @@
           </li> --}}
 
           <li>
-            <!-- Dropdown Menu -->
-            <x-sidebar-dropdown id="category" name="Categori">
-                {{-- <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"></path> --}}
-                <i class="fa-regular fa-file-lines"></i>
-                <x-slot name="content">
-                    <li>
-                        <x-sidebar-link-dropdown link="{{ route('admin.category.index') }}" name="Categori" />
-                    </li>
-                    <li>
-                        <x-sidebar-link-dropdown link="{{ route('admin.sub_categories.index') }}" name="Sub Categori" />
-                    </li>
-                </x-slot>
-            </x-sidebar-dropdown>
+            <x-sidebar-link link="{{ route('admin.combined-categories.index') }}" token="combined-categories" name="Category">
+              <i class="fa-regular fa-file-lines"></i>
+            </x-sidebar-link>
           </li>
 
           <li>
@@ -182,7 +172,7 @@
           </li>
         </ul>
         <div class="pt-2 space-y-2">
-          <x-sidebar-link link="{{ route('profile.edit') }}"  name="Profile">
+          <x-sidebar-link link="{{ route('profile.edit') }}" token="profile" name="Profile">
             <i class="fa-solid fa-user"></i>
           </x-sidebar-link>
           <form method="POST" action="{{ route('logout') }}" class="">
